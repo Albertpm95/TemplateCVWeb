b7c5108da91ddbcf91ddba48ee8426e6b5b2d078
albertpm95 <albertpm95@outlook.com>
2023-04-13 10:05:42 +0200

    Directive to open external links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
+import { ExternalLinkDirective } from './directives/external-link.directive';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    ComponentsModule
-  ],
+  declarations: [AppComponent, ExternalLinkDirective],
+  imports: [BrowserModule, ComponentsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
