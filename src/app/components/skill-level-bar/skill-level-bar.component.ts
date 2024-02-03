8df56ebc55e8aa95e87db6a674026c9120cca8be
Albert <albertpm95@gmail.com>
2024-02-03 11:20:15 +0100

    Eliminados spec Actualizado angular Añadido prettier

--- src/app/components/skill-level-bar/skill-level-bar.component.ts
+++ src/app/components/skill-level-bar/skill-level-bar.component.ts
@@ -2,7 +2,21 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'skill-level-bar',
-  templateUrl: './skill-level-bar.component.html',
+  template: `
+  <div class="bar-container">
+  <span class="name" *ngIf="name"><strong>{{ name }}</strong></span><br>
+  <small class="subname" *ngIf="subname">{{subname}}</small>
+  <div class="bar" *ngIf="completed">
+    <div
+      class="bar-level"
+      [ngStyle]="{'background-color': (i < completed) ? color : ''}"
+      *ngFor="let level of [].constructor(divisions); let i = index;"
+    ></div>
+  </div>
+  <span class="label" *ngIf="label">{{ label }}</span>
+</div>
+
+  `,
   styleUrls: ['./skill-level-bar.component.sass'],
 })
 export class SkillLevelBarComponent {
@@ -13,5 +27,5 @@ export class SkillLevelBarComponent {
   @Input() completed = 0;
   @Input() color = 'green';
 
-  constructor() {}
+  constructor() { }
 }
